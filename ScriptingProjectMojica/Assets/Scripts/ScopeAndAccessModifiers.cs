663afe67d1d03427a025a794a40d06041e70bc0f
JJmojica23 <jjmojica2015@gmail.com>
2022-11-07 10:54:01 -0800

    Added ScopeAndAccessModifiers
    Worked on and finished ScopeAndAccessModifiers scripting project. Two scripts were added named ScopeAndAccessModifiers and AnotherClass.

--- ScriptingProjectMojica/Assets/Scripts/ScopeAndAccessModifiers.cs
+++ ScriptingProjectMojica/Assets/Scripts/ScopeAndAccessModifiers.cs
@@ -11,28 +11,6 @@ public class ScopeAndAccessModifiers : MonoBehaviour
 
     private AnotherClass myOtherClass;
 
-    public class AnotherClass
-    {
-        public int apples;
-        public int bananas;
-
-        private int stapler;
-        private int sellotape;
-
-        public void FruitMachine (int a, int b)
-        {
-            int answer;
-            answer = a + b;
-            Debug.Log("Fruit total; " + answer);
-        }
-
-        private void OfficeSort (int a, int b)
-        {
-            int answer;
-            answer = a + b;
-            Debug.Log("Office Supplies total: " + answer);
-        }
-    }
     // Start is called before the first frame update
     void Start()
     {
